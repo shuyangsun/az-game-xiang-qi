583ba0c0c46d755c71985886cb14e94888e431e4
Shuyang Sun <shuyangsun10@gmail.com>
2026-05-14 21:44:54 -0400

    Add TanStack Start + Bun GUI template

---
+++ gui/eslint.config.js
@@ -0,0 +1,25 @@
+//  @ts-check
+
+import { tanstackConfig } from '@tanstack/eslint-config'
+import reactCompiler from 'eslint-plugin-react-compiler'
+
+export default [
+  ...tanstackConfig,
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'import/no-cycle': 'off',
+      'import/order': 'off',
+      'sort-imports': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/require-await': 'off',
+      'pnpm/json-enforce-catalog': 'off',
+      'react-compiler/react-compiler': 'error',
+    },
+  },
+  {
+    ignores: ['eslint.config.js', 'prettier.config.js'],
+  },
+]
